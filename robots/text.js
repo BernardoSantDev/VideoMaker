cb6f052050c68d07bee59e816dec636b6814c0f7
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-26 00:06:46 -0300

    Autentica, busca, usa e retorna o valor do algoritimo

--- robots/text.js
+++ robots/text.js
@@ -1,7 +1,17 @@
+const algorithmia = require('algorithmia')
+
 function robot(content) {
     fetchContentFromWikipedia(content)
-    sanitizeContent(content)
-    beakContentIntoSentences(content)
+    //sanitizeContent(content)
+    //beakContentIntoSentences(content)
+
+    function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticatad = algorithmia('API KEY TEMPORARIA')
+        const wikipediaAlgorithm = algorithmiaAuthenticatad.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
+        console.log(wikipediaContent)
+    }
 }
 
 module.exports = robot
