d9180a817019fa47d2a5d47b70dcfab4b4d45958
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-27 23:41:48 -0300

    cria robo do input

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-const readline = require('readline-sync')
 const robots = {
+    input: require('./robots/input.js'),
     text: require('./robots/text.js')
 }
 
 async function start() {
-    const content = {
-        maximumSentences: 7
-    }
+    
+    robots
+    //await robots.text(content)
 
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-
-    await robots.text(content)
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
-    }
-
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        
-        return selectedPrefixText
-    }
-
-    console.log(JSON.stringify(content, null, 4))
 }
 
 start();
