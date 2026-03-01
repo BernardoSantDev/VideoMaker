b79f67652ba7f38561959cc9301a0faba329f922
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-03-01 15:51:09 -0300

    finaliza o dowload de imagens

--- robots/image.js
+++ robots/image.js
@@ -8,10 +8,10 @@ const pexelsCredentials = require('../credentials/pexels.json')
 async function robot() {
     const content = state.load()
 
-    //await fetchImagesOfAllSentences(content)
+    await fetchImagesOfAllSentences(content)
     await downloadAllImages(content)
 
-    //state.save(content)
+    state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
