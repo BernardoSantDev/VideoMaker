f264556d303e07fa41f1eb980f6bca72d574d381
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-28 23:08:33 -0300

    pega uma array com links

--- robots/image.js
+++ robots/image.js
@@ -5,18 +5,27 @@ const pexelsCredentials = require('../credentials/pexels.json')
 async function robot() {
     const content = state.load()
 
-    const response = await axios.get('https://api.pexels.com/v1/search', {
-        headers: {
-            Authorization: pexelsCredentials.apiKey
-        },
-        params: {
-            query: 'Michael Jackson',
-            per_page: 2
-        }
-    })
-
-    console.dir(response.data, { depth: null })
+    const imagesArray = await fetchImagesAndReturnImagesLinks("Michael Jackson")
+    console.dir(imagesArray, { depth: null })
     process.exit(0)
+
+    async function fetchImagesAndReturnImagesLinks(query) {
+        const response = await axios.get('https://api.pexels.com/v1/search', {
+            headers: {
+                Authorization: pexelsCredentials.apiKey
+            },
+            params: {
+                query: query,
+                per_page: 2
+            }
+        })
+
+        const imagesUrl = response.data.photos.map((photo) => {
+            return photo.src.medium
+        })
+
+        return imagesUrl
+    }
 }
 
 module.exports = robot
