5d8ccbbf8258f08f68f0e4990be0ae0d7fb59bf4
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-03-03 15:29:47 -0300

    cria o startWebServer

--- robots/youtube.js
+++ robots/youtube.js
@@ -1,5 +1,8 @@
 const { auth } = require('googleapis/build/src/apis/youtube')
 const state = require('./state.js')
+const express = require('express')
+const google = require('googleapis').google
+const OAuth2 = google.auth.OAuth2
 
 async function robot() {
     const content = state.load()
@@ -16,6 +19,22 @@ async function robot() {
         //await requestGoogleForAccessTokens()
         //await setGlobalGoogleAuthentication()
         //await stopWebServer()
+
+        async function startWebServer() {
+            return new Promise((resolve, reject) => {
+                const port = 5000
+                const app = express()
+
+                const server = app.listen(port, () => {
+                    console.log(`> [youtube-robot] Listening on http://localhost:${port}`)
+
+                    resolve({
+                        app,
+                        server
+                    })
+                })
+            })
+        }
         
     }
 
